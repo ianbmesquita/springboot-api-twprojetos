51ea5a5402b2f0acb5d9c9160e065856016dff3a
ianbmesquita <ianmesquitadb@gmail.com>
2022-06-30 01:33:44 -0300

    feat: Creating the GET route of Cargo in API.

---
+++ src/main/java/br/com/treinaweb/twprojetos/api/controllers/CargoApiController.java
@@ -0,0 +1,24 @@
+package br.com.treinaweb.twprojetos.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.treinaweb.twprojetos.entities.Cargo;
+import br.com.treinaweb.twprojetos.services.CargoService;
+
+@RestController
+@RequestMapping("/api/v1/cargos")
+public class CargoApiController {
+    @Autowired
+    private CargoService cargoService;
+
+    @GetMapping
+    public List<Cargo> findAll() {
+        return cargoService.findAll();
+    }
+    
+}
