96745830142d918e5e6d126ebcfd45cab014a95d
ianbmesquita <ianmesquitadb@gmail.com>
2022-06-30 01:45:58 -0300

    feat: Creating the GET endpoint to find a Cargo by id.

--- src/main/java/br/com/treinaweb/twprojetos/api/controllers/CargoApiController.java
+++ src/main/java/br/com/treinaweb/twprojetos/api/controllers/CargoApiController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class CargoApiController {
     @GetMapping
     public List<Cargo> findAll() {
         return cargoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cargo findById(@PathVariable Long id) {
+        return cargoService.findById(id);
     }
     
 }
