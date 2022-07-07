cd7f88a4a2f3562af9cdfe7cf0cd61d0a2dd6e0e
ianbmesquita <ianmesquitadb@gmail.com>
2022-07-06 23:14:13 -0300

    feat: Creating HATEOAS links to Cargo.

--- src/main/java/br/com/treinaweb/twprojetos/api/controllers/CargoApiController.java
+++ src/main/java/br/com/treinaweb/twprojetos/api/controllers/CargoApiController.java
@@ -1,11 +1,12 @@
 package br.com.treinaweb.twprojetos.api.controllers;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.treinaweb.twprojetos.api.dtos.CargoDTO;
+import br.com.treinaweb.twprojetos.api.hateoas.CargoAssembler;
 import br.com.treinaweb.twprojetos.entities.Cargo;
 import br.com.treinaweb.twprojetos.services.CargoService;
 
@@ -26,31 +28,36 @@ public class CargoApiController {
     @Autowired
     private CargoService cargoService;
 
+    @Autowired
+    private CargoAssembler cargoAssembler;
+
     @GetMapping
-    public List<Cargo> findAll() {
-        return cargoService.findAll();
+    public CollectionModel<EntityModel<Cargo>> findAll() {
+        return cargoAssembler.toCollectionModel(cargoService.findAll());
     }
 
     @GetMapping("/{id}")
-    public Cargo findById(@PathVariable Long id) {
-        return cargoService.findById(id);
+    public EntityModel<Cargo> findById(@PathVariable Long id) {
+        return cargoAssembler.toModel(cargoService.findById(id));
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Cargo save(@RequestBody @Valid CargoDTO cargoDTO) {
-        return cargoService.save(cargoDTO);
+    public EntityModel<Cargo> save(@RequestBody @Valid CargoDTO cargoDTO) {
+        return cargoAssembler.toModel(cargoService.save(cargoDTO));
     }
 
     @PutMapping("/{id}")
-    public Cargo update(@RequestBody @Valid CargoDTO cargoDTO, @PathVariable Long id) {
-        return cargoService.update(cargoDTO, id);
+    public EntityModel<Cargo> update(@RequestBody @Valid CargoDTO cargoDTO, @PathVariable Long id) {
+        return cargoAssembler.toModel(cargoService.update(cargoDTO, id));
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         cargoService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
-    
+
 }
---
+++ src/main/java/br/com/treinaweb/twprojetos/api/hateoas/CargoAssembler.java
@@ -0,0 +1,55 @@
+package br.com.treinaweb.twprojetos.api.hateoas;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import br.com.treinaweb.twprojetos.api.controllers.CargoApiController;
+import br.com.treinaweb.twprojetos.entities.Cargo;
+
+@Component
+public class CargoAssembler implements SimpleRepresentationModelAssembler<Cargo> {
+
+    @Override
+    public void addLinks(EntityModel<Cargo> resource) {
+        Long id = resource.getContent().getId();
+
+        Link selfLink = linkTo(methodOn(CargoApiController.class).findById(id))
+            .withSelfRel()
+            .withType("GET")
+            .withTitle("Exibe os detalhes do cargo.");
+        
+        Link updateLink = linkTo(methodOn(CargoApiController.class).update(null, id))
+            .withSelfRel()
+            .withType("PUT")
+            .withTitle("Atualiza os detalhes do cargo.");
+
+        Link deleteLink = linkTo(methodOn(CargoApiController.class).delete(id))
+            .withSelfRel()
+            .withType("DELETE")
+            .withTitle("Exclui o cargo.");
+        
+        resource.add(selfLink, updateLink, deleteLink);
+    }
+
+    @Override
+    public void addLinks(CollectionModel<EntityModel<Cargo>> resources) {
+        Link selfLink = linkTo(methodOn(CargoApiController.class).findAll())
+            .withSelfRel()
+            .withType("GET")
+            .withTitle("Busca todos os cargos cadastrados.");
+
+        Link saveLink = linkTo(methodOn(CargoApiController.class).save(null))
+            .withSelfRel()
+            .withType("POST")
+            .withTitle("Cadastra um novo cargo.");
+        
+        resources.add(selfLink, saveLink);
+    }
+    
+}
